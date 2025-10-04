256cb6cc91128eaf34b1bbdda9e6897cbc7f9f2a
jakubwrobel2003 <jakubwrobel20030404@gmail.com>
2025-10-04 22:57:53 +0200

    Initial commit - Spring Boot project Gdziedrinki

---
+++ src/test/java/org/example/gdziedrinki/GdziedrinkiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gdziedrinki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdziedrinkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
